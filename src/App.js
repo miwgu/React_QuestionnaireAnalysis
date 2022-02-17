633c4c25c4abcf42fa3228c2d86d9d8d076964e1
Miwa G <miwa.guhres@hotmail.com>
2022-02-17 15:42:14 +0100

    add new folder

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import Navbar from './components/Navbar';
+import Navbar from './components/nav/Navbar';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './category_pages/Home';
 import Psoriasis_1 from './category_pages/Psoriasis_1';
